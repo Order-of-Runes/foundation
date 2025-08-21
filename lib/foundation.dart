101bb1b289ed800f585836e41e2cd17e6f3fa8ca
ishowrunes <ishworisgod@gmail.com>
2025-08-21 13:05:03 +0545

    Remove all concrete logic from state_foundation

--- lib/foundation.dart
+++ lib/foundation.dart
@@ -1,6 +1,4 @@
 // Copyright (c) 2025 Order of Runes Authors. All rights reserved.
 
-export 'src/failure_foundation.dart';
 export 'src/network_info_foundation.dart';
 export 'src/state_foundation.dart';
-export 'src/view_model_foundation.dart';
--- lib/src/state_foundation.dart
+++ lib/src/state_foundation.dart
@@ -1,79 +1,21 @@
 // Copyright (c) 2025 Order of Runes Authors. All rights reserved.
 
 import 'package:equatable/equatable.dart';
-import 'package:foundation/src/failure_foundation.dart';
 
 abstract class StateFoundation extends Equatable {
-  const StateFoundation({
-    this.failure,
-    this.loading = Loading.inline,
-    this.failureDisplay = FailureDisplay.inline,
-    this.loadingTitle,
-    this.loadingSubtitle,
-    this.canDismissLoading = false,
-  });
+  const StateFoundation();
 
-  final FailureFoundation? failure;
-  final Loading loading;
-  final FailureDisplay failureDisplay;
-  final String? loadingTitle;
-  final String? loadingSubtitle;
+  bool get hasFailed;
 
-  /// Can loading be dismissed by the user
-  ///
-  /// Only applies to [Loading.progressDialog]
-  final bool canDismissLoading;
+  bool get isLoading;
 
-  StateFoundation setFailure(FailureFoundation failure);
+  bool get hasLoaded => !hasFailed && !isLoading;
 
-  StateFoundation setFailureDisplay(FailureDisplay display);
+  String? get loadingTitle => null;
 
-  StateFoundation setLoading({
-    Loading loading = Loading.inline,
-    String? title,
-    String? subtitle,
-    bool canDismissLoading = false,
-  });
+  String? get loadingSubtitle => null;
 
-  bool get hasFailed => failure != null;
+  String? get failureMessage => null;
 
-  bool get hasLoaded => loading == Loading.none && !hasFailed;
-}
-
-/// Dictates how loading should be shown in the UI
-/// LoadingDisplay.inline
-/// LoadingDisplay.top
-enum Loading {
-  /// Show loading in the page itself
-  ///
-  /// For cases when a page itself is not yet ready to display content
-  inline,
-
-  /// Show loading at the end of a list
-  ///
-  /// For cases when pagination is required for a list
-  pagination,
-
-  /// Use this for cases when the page is ready and is visible.
-  /// If the page is not yet ready or the user needs to be shown that
-  /// the page is building, go with [Loading.inline]
-  dialog,
-
-  /// No loading is to be shown
-  none,
-}
-
-/// Dictates how a failure is to be displayed in the UI
-/// FailureDisplay.inline
-/// FailureDisplay.popup
-/// FailureDisplay.snackBar
-enum FailureDisplay {
-  /// Show failure in a popup modal (Dialog)
-  dialog,
-
-  /// Show failure in the page itself
-  inline,
-
-  /// Show failure in a snack bar
-  snackBar,
+  String? get failureDetail => null;
 }
