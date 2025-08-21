f3051feb997a9d6730b6caea8b9cb0377dc67686
ishowrunes <ishworisgod@gmail.com>
2025-08-21 13:04:40 +0545

    Remove unnecessary view model and failure foundation

--- lib/src/view_model_foundation.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025 Order of Runes Authors. All rights reserved.
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:foundation/src/state_foundation.dart';
-
-abstract class ViewModelFoundation<S extends StateFoundation> extends AutoDisposeNotifier<S> {}
-
-abstract class StickyViewModelFoundation<S extends StateFoundation> extends Notifier<S> {}
